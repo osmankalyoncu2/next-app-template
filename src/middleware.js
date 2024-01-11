22e89b91dc744fc8bab0289eaa90c87388b6c436
Arsen Shkrumelyak <arsen@nerverift.com>
2024-01-11 19:38:01 +0000

    update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -40,15 +40,18 @@ export default auth((req) => {
     }
 })
 
+/*
+ * Match all request paths except for the ones starting with:
+ * - api (API routes)
+ * - _next/static (static files)
+ * - _next/image (image optimization files)
+ * - favicon.ico (favicon file)
+ */
+
+/*
 export const config = {
     matcher: [
-        /*
-         * Match all request paths except for the ones starting with:
-         * - api (API routes)
-         * - _next/static (static files)
-         * - _next/image (image optimization files)
-         * - favicon.ico (favicon file)
-         */
         '/((?!api|_next/static|_next/image|favicon.ico).*)',
     ],
 }
+*/
